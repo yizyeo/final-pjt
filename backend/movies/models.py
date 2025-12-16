3b1bbc226e644ad165980598ab5b047fb7eccec7
bsson0502 <bsson0502@gmail.com>
2025-12-16 13:08:42 +0900

    feat: moviess, reviews 모델 작성 (#2)

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Movie(models.Model):
+  movie_id = models.IntegerField()
+  genre = models.IntegerField()
+  title = models.CharField()
+  poster = models.CharField()
+  release_date = models.CharField()
+
+class Genre(models.Model):
+  name = models.CharField()
+  genre_id = models.IntegerField()
+  movie_id = models.ForeignKey()
