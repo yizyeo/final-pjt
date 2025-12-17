9c46da6b3acd8f3bdfb1dc552de17db5a5eed6b3
yizyeo <yizyeo@gmail.com>
2025-12-18 01:38:25 +0900

    chore: api_url 환경변수로 설정 (#1)

--- frontend/src/stores/accounts.js
+++ frontend/src/stores/accounts.js
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router'
 import axios from 'axios'
 
 export const useAccountStore = defineStore('account', () => {
-  const API_URL = 'http://127.0.0.1:8000'
+  const API_URL = import.meta.env.VITE_API_URL
   const token = ref(null)
 
   const router = useRouter()
