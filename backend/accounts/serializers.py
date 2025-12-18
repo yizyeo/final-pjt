6faef596573e7c14099daf283f92c867c698b9f0
yizyeo <yizyeo@gmail.com>
2025-12-18 17:05:52 +0900

    feat: 회원정보 필수 입력 필드 설정 (#5)

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,25 +3,27 @@ from rest_framework import serializers
 from .models import Genre, User
 
 class CustomRegisterSerializer(RegisterSerializer):
-    age = serializers.IntegerField(required=False)
+    email = serializers.EmailField(required=True)
+    age = serializers.IntegerField(required=True)
+    gender = serializers.ChoiceField(
+        choices=[('M', '남성'), ('F', '여성')],
+        required=True
+    )
     favorite_genres = serializers.ListField(
         child=serializers.IntegerField(),
-        required=False
+        required=True,
+        allow_empty=False
     )
 
     def save(self, request):
         user = super().save(request)
 
-        # age 저장
-        user.age = self.validated_data.get('age')
+        user.age = self.validated_data['age']
+        user.gender = self.validated_data['gender']
         user.save()
 
-        # 선호 장르 저장
-        genre_ids = self.validated_data.get('favorite_genres', [])
-        print(f'수신된 장르 ID 목록: {genre_ids}') # 터미널에서 확인용
-        if genre_ids:
-            genres = Genre.objects.filter(genre_id__in=genre_ids)
-            print(f'조회된 장르 객체들: {genres}') # 필터링이 잘 되었는지 확인
-            user.favorite_genres.set(genres)
+        genre_ids = self.validated_data['favorite_genres']
+        genres = Genre.objects.filter(genre_id__in=genre_ids)
+        user.favorite_genres.set(genres)
 
         return user
