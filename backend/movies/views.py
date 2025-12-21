27018c5edd05d4d468e448989483dee0350b71fd
bottle-siu <bsson0502@naver.com>
2025-12-21 18:18:53 +0900

    feat: 영화 전체 목록 구성 및 데이터 (#25)

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Genre, Movie
-from .serializers import GenreSerializer, HomeBackdropSerializer, HomeListSerializer, MovieDetailSerializer
+from .serializers import (GenreSerializer, 
+                          HomeBackdropSerializer, 
+                          HomeListSerializer, 
+                          MovieDetailSerializer, 
+                          MovieListSerializer,)
+from django.utils import timezone
+from django.db.models import Case, When, Value, IntegerField
 
 @api_view(['GET'])
 def genre_list(request):
@@ -34,5 +40,26 @@ def movie_like():
     pass
 
 @api_view(['GET'])
-def movie_list():
-    pass
+def movie_list(request):
+    now = timezone.now()
+    # 개봉일만 오늘 이전인 영화
+    # movies = Movie.objects.filter(release_date__lte=now).order_by('-release_date')[:20]
+    # serializer = MovieListSerializer(movies, many=True)
+    # return Response(serializer.data)
+
+    # 개봉일 + now_playing 우선
+    movies = Movie.objects.filter(release_date__lte=now)
+
+    movies = movies.annotate(
+        priority=Case(
+            When(list_type='now_playing', then=Value(0)),
+            default=Value(1),
+            output_field=IntegerField(),
+        )
+    ).order_by('priority', '-release_date')[:20] # 일단 임시로 20개만
+
+    serializer = MovieListSerializer(movies, many=True)
+    return Response(serializer.data)
+
+    # 확인 결과 큰 차이 없음 popularity를 가져와서 조건 추가하는게...
+    
