8485821d91bb8d6b9354bf8b86e72a76b8e6d109
yizyeo <yizyeo@gmail.com>
2025-12-22 12:21:21 +0900

    feat: 리뷰 페이지 관련  초기 설정 (#28)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,30 +2,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAccountStore } from '@/stores/accounts'
 import HomeView from '@/views/HomeView.vue'
 import SignUpView from '@/views/SignUpView.vue'
-import LogInView from '@/views/LoginView.vue'
+import LogInView from '@/views/LogInView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import MovieDetailView from '@/views/MovieDetailView.vue'
 import MovieListView from '@/views/MovieListView.vue'
+import ReviewListView from '@/views/ReviewListView.vue'
+import ReviewDetailView from '@/views/ReviewDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // 메인 페이지 (기본 페이지)
     {
       path: '/',
       name: 'HomeView',
       component: HomeView
     },
-
+    // 회원가입 페이지
     {
       path: '/signup',
       name: 'SignUpView',
       component: SignUpView
     },
+    // 로그인 페이지
     {
       path: '/login',
       name: 'LogInView',
       component: LogInView
     },
+    // 사용자 개인 페이지 (마이페이지)
     {
       path: '/profile/:username',
       name: 'ProfileView',
@@ -40,18 +45,24 @@ const router = createRouter({
       path: '/movies/movielist',
       name: 'MovieListView',
       component: MovieListView
-    }
+    },
+    // 전체 리뷰 페이지
+    {
+      path: '/reviews',
+      name: 'ReviewList',
+      component: ReviewListView
+    },
+    // 리뷰 상세 페이지
+    {
+      path: '/reviews/:reviewId',
+      name: 'ReviewDetail',
+      component: ReviewDetailView
+    },
   ]
 })
 
 router.beforeEach((to, from) => {
   const accountStore = useAccountStore()
-
-  // 게시물 접근 제한
-  // if (to.name === 'ArticleView' && !accountStore.isLogin) {
-  //   window.alert('로그인이 필요합니다.')
-  //   return { name: 'LogInView' }
-  // }
 
   if ((to.name === 'SignUpView' || to.name === 'LogInView') && (accountStore.isLogin) ) {
     window.alert('이미 로그인 되어 있습니다.')
